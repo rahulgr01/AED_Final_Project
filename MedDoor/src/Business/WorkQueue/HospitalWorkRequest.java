71c53438b748f628e4914da7bca22a4181f28347
GursheenKaurNEU <kaur.gurs@northeastern.edu>
2022-12-09 22:52:24 -0500

    Adding Hospital Enterprise Admin's Page,  Adding work request classes
    Adding Hospital Enterprise Admin's Page,
    containing staff and doctor organization.
    Creating work request classes for connecting all the organizations

---
+++ MedDoor/src/Business/WorkQueue/HospitalWorkRequest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author Gursheen Kaur
+ */
+public class HospitalWorkRequest  extends WorkRequest{
+    
+    
+    
+}
